66658cd50d4c3a333ea1605f23021debdcc334dc
Kirk Roerig <mr.possoms@gmail.com>
2019-07-04 18:00:18 -0600

    Inital commit

---
+++ hw/gpio.c
@@ -0,0 +1,9 @@
+#include "gpio.h"
+
+
+int hwd_gpio_set(int pin, int state)
+{
+	
+
+	return 0;
+}
---
+++ hw/gpio.h
@@ -0,0 +1,6 @@
+#ifndef HWD_GPIO_H
+#define HWD_GPIO_H
+
+int hwd_gpio_set(int pin, int state);
+
+#endif
