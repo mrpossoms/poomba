8f0244f0d444c24a70ee5be76071d9c5cc982767
Kirk Roerig <mr.possoms@gmail.com>
2019-09-02 16:33:06 -0600

    Implemented laplacian variance in C to run on the rpi zero before tranmitting a frame

--- hw/hwd.c
+++ hw/hwd.c
@@ -12,6 +12,7 @@
 #include "gpio.h"
 #include "camera.h"
 
+#define BLUR_THRESHOLD 0
 #define CAM_WIDTH 640
 #define CAM_HEIGHT 480
 
@@ -20,16 +21,17 @@ typedef union {
 		uint8_t r, g, b;
 	};
 	uint8_t v[3];
-} rgb_t __attribute__ ((packed));
+} rgb_t;
 
 typedef struct {
 	rgb_t pixels[CAM_HEIGHT][CAM_WIDTH];
-} frame_t __attribute__ ((packed));
+} frame_t;
 
 typedef struct {
 	char magic[4];
 	int32_t width, height, depth;
-} hdr_t __attribute__ ((packed));
+} hdr_t;
+
 
 int request_classification(const char* host_name, frame_t* frame, uint32_t* is_ok)
 {
@@ -67,7 +69,7 @@ int request_classification(const char* host_name, frame_t* frame, uint32_t* is_o
 	for (size_t written = 0; written < sizeof(frame_t);)
 	{
 		int bytes = write(sock, buf + written, sizeof(frame_t) - written);
-		
+
 		if (bytes < 0)
 		{
 			res = -4;
@@ -87,6 +89,50 @@ abort:
 	if (res) { fprintf(stderr, "error %d: %s\n", res, strerror(errno)); }
 	close(sock);
 	return res;
+}
+
+
+unsigned long variance(int mu, const int* v, size_t n)
+{
+	unsigned long var = 0;
+	for (size_t i = 0; i < n; ++i)
+	{
+		int d = v[i] - mu;
+		var += d * d;
+	}
+	return var / (n - 1);
+}
+
+
+void leplacian(frame_t* frame, int* L, long* mu)
+{
+	const int K[3][3] = {
+		{ 0, 1, 0 },
+		{ 1,-4, 1 },
+		{ 0, 1, 0 },
+	};
+
+	*mu = 0;
+
+	size_t l_c = CAM_WIDTH - 2;
+	size_t l_r = CAM_HEIGHT - 2;
+
+	for (int r = 0; r < l_r; ++r)
+	for (int c = 0; c < l_c; ++c)
+	{
+		int inner = 0;
+		for (int k_r = 0; k_r < 3; k_r++)
+		for (int k_c = 0; k_c < 3; k_c++)
+		{
+			inner += K[k_r][k_c] * frame->pixels[r + k_r][c + k_c].r;
+		}
+
+		L[r * l_c + c] = inner;
+
+		*mu += inner;
+	}
+
+	*mu /= l_c * l_r;
 }
 
 
@@ -131,7 +177,9 @@ int main (int argc, const char* argv[])
 	cam_settings_t cam_cfg = { CAM_WIDTH, CAM_HEIGHT, 30 };
 	cam_t cam = cam_open("/dev/video0", &cam_cfg);
 	frame_t frames[2];
-	
+
+	int L[CAM_HEIGHT - 2][CAM_WIDTH - 2];
+
 	for (unsigned int i = 0; 1; ++i)
 	{
 		int frame_idx = i % 2;
@@ -149,7 +197,16 @@ int main (int argc, const char* argv[])
 		}
 
 		uint32_t is_ok = 0;
-		request_classification(argv[1], frames + frame_idx, &is_ok);
+		frame_t* current_frame = frames + frame_idx;
+
+		long mu = 0;
+		leplacian(current_frame, (int*)L, &mu);
+		if (variance(mu, (int*)L, sizeof(L) / sizeof(int)) < BLUR_THRESHOLD)
+		{
+
+		}
+
+		request_classification(argv[1], current_frame, &is_ok);
 		hwd_gpio_set(4, is_ok);
 	}
 
