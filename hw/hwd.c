8531f1d6729e80c0efe401ef8b0eacc5442e976d
Kirk Roerig <mr.possoms@gmail.com>
2019-10-14 06:43:21 +0100

    hwd downsample and leplacian variance rejection

--- hw/hwd.c
+++ hw/hwd.c
@@ -13,8 +13,16 @@
 #include "camera.h"
 
 #define BLUR_THRESHOLD 0
-#define CAM_WIDTH 640
-#define CAM_HEIGHT 480
+#define MIN_IMG_DIFF 0
+#define CAM_WIDTH (640 >> 0)
+#define CAM_HEIGHT (480 >> 0)
+#define DS_WIDTH  (640 >> 1)
+#define DS_HEIGHT (480 >> 1)
+
+#define EACH_ROW_COL(_R, _C) for (int r = 0; r < _R; ++r) \
+                             for (int c = 0; c < _C; ++c) \
+
+typedef struct { size_t r, c; } dims_t;
 
 typedef union {
 	struct {
@@ -28,12 +36,16 @@ typedef struct {
 } frame_t;
 
 typedef struct {
+	rgb_t pixels[DS_HEIGHT][DS_WIDTH];
+} ds_frame_t;
+
+typedef struct {
 	char magic[4];
 	int32_t width, height, depth;
 } hdr_t;
 
 
-int request_classification(const char* host_name, frame_t* frame, uint32_t* is_ok)
+int request_classification(const char* host_name, ds_frame_t* frame, uint32_t* is_ok)
 {
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
 	int res = 0;
@@ -58,8 +70,8 @@ int request_classification(const char* host_name, frame_t* frame, uint32_t* is_o
 	// send the header
 	hdr_t hdr = {
 		.magic = "POOP",
-		.width = CAM_WIDTH,
-		.height = CAM_HEIGHT,
+		.width = DS_WIDTH,
+		.height = DS_HEIGHT,
 		.depth = 3,
 	};
 	if (write(sock, &hdr, sizeof(hdr_t)) != sizeof(hdr_t)) { res = -3; goto abort; }
@@ -92,9 +104,9 @@ abort:
 }
 
 
-unsigned long variance(int mu, const int* v, size_t n)
+float variance(float mu, const int* v, size_t n)
 {
-	unsigned long var = 0;
+	float var = 0;
 	for (size_t i = 0; i < n; ++i)
 	{
 		int d = v[i] - mu;
@@ -104,7 +116,7 @@ unsigned long variance(int mu, const int* v, size_t n)
 }
 
 
-void leplacian(frame_t* frame, int* L, long* mu)
+void leplacian(ds_frame_t* frame, int* L, float* mu, dims_t dims)
 {
 	const int K[3][3] = {
 		{ 0, 1, 0 },
@@ -114,8 +126,8 @@ void leplacian(frame_t* frame, int* L, long* mu)
 
 	*mu = 0;
 
-	size_t l_c = CAM_WIDTH - 2;
-	size_t l_r = CAM_HEIGHT - 2;
+	size_t l_c = dims.c - 2;
+	size_t l_r = dims.r - 2;
 
 	for (int r = 0; r < l_r; ++r)
 	for (int c = 0; c < l_c; ++c)
@@ -124,7 +136,8 @@ void leplacian(frame_t* frame, int* L, long* mu)
 		for (int k_r = 0; k_r < 3; k_r++)
 		for (int k_c = 0; k_c < 3; k_c++)
 		{
-			inner += K[k_r][k_c] * frame->pixels[r + k_r][c + k_c].r;
+			rgb_t rgb = frame->pixels[r+k_r][c+k_c];
+			inner += K[k_r][k_c] * rgb.r;//(rgb.r + rgb.g + rgb.b) / 3;
 		}
 
 		L[r * l_c + c] = inner;
@@ -132,7 +145,7 @@ void leplacian(frame_t* frame, int* L, long* mu)
 		*mu += inner;
 	}
 
-	*mu /= l_c * l_r;
+	*mu /= (l_c * l_r);
 }
 
 
@@ -156,13 +169,25 @@ void get_frame(cam_t const* cam, frame_t* frame)
 unsigned int frame_diff(frame_t* f0, frame_t* f1)
 {
 	unsigned int diff = 0;
-	for (int r = 0; r < CAM_HEIGHT; ++r)
-	for (int c = 0; c < CAM_WIDTH; ++c)
+
+	EACH_ROW_COL(CAM_HEIGHT, CAM_WIDTH)
 	{
 		diff += abs(f0->pixels[r][c].r - f1->pixels[r][c].r);
 	}
 
 	return diff / (CAM_WIDTH * CAM_HEIGHT);
+}
+
+
+void downsample_frame(ds_frame_t* ds_frame, frame_t* frame)
+{
+	int dr = CAM_HEIGHT / DS_HEIGHT;
+	int dc = CAM_WIDTH / DS_WIDTH;
+
+	EACH_ROW_COL(DS_HEIGHT, DS_WIDTH)
+	{
+		ds_frame->pixels[r][c] = frame->pixels[r * dr][c * dc];
+	}
 }
 
 
@@ -178,7 +203,7 @@ int main (int argc, const char* argv[])
 	cam_t cam = cam_open("/dev/video0", &cam_cfg);
 	frame_t frames[2];
 
-	int L[CAM_HEIGHT - 2][CAM_WIDTH - 2];
+	int L[DS_HEIGHT - 2][DS_WIDTH - 2];
 
 	for (unsigned int i = 0; 1; ++i)
 	{
@@ -187,26 +212,33 @@ int main (int argc, const char* argv[])
 		cam_wait_frame(&cam);
 		get_frame(&cam, frames + frame_idx);
 
+		/*
 		if (i % 30) { continue; }
 
 		if (i > 1)
 		{
 			int diff = frame_diff(frames + 0, frames + 1);
 			fprintf(stderr, "frame_diff: %d\n", diff);
-			if (diff < 5) { continue; }
+			if (diff < MIN_IMG_DIFF) { continue; }
 		}
+		*/
 
 		uint32_t is_ok = 0;
+		float mu = 0;
 		frame_t* current_frame = frames + frame_idx;
+		ds_frame_t ds_frame;
 
-		long mu = 0;
-		leplacian(current_frame, (int*)L, &mu);
-		if (variance(mu, (int*)L, sizeof(L) / sizeof(int)) < BLUR_THRESHOLD)
+		downsample_frame(&ds_frame, current_frame);
+		leplacian(&ds_frame, (int*)L, &mu, (dims_t){ DS_HEIGHT, DS_WIDTH });
+		int var = variance(mu, (int*)L, sizeof(L) / sizeof(int));
+		printf("mu: %f, variance: %d\n", mu, var);
+
+		if (var < BLUR_THRESHOLD)
 		{
 
 		}
 
-		request_classification(argv[1], current_frame, &is_ok);
+		request_classification(argv[1], &ds_frame, &is_ok);
 		hwd_gpio_set(4, is_ok);
 	}
 
